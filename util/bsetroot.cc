4e2c37f2d20a1be4fd7826b817ec5b7caa31138a
Dana Jansens <danakj@orodu.net>
2002-05-05 23:18:35 +0000

    fix headers to use the new defines

--- util/bsetroot.cc
+++ util/bsetroot.cc
@@ -2,10 +2,13 @@
 #  include "../config.h"
 #endif // HAVE_CONFIG_H
 
-#ifdef    STDC_HEADERS
-#  include <string.h>
+#ifdef    HAVE_STDLIB_H
 #  include <stdlib.h>
-#endif // STDC_HEADERS
+#endif // HAVE_STDLIB_H
+
+#ifdef    HAVE_STRING_H
+#  include <string.h>
+#endif // HAVE_STRING_H
 
 #ifdef    HAVE_STDIO_H
 #  include <stdio.h>

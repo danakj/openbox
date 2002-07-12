89d09297f0ef3d2db648a756f62a57bf2be317df
Scott Moynes <smoynes@nexus.carleton.ca>
2002-07-12 03:03:55 +0000

    some of the actions I came up with

---
+++ util/epist/actions.hh
@@ -0,0 +1,24 @@
+// xOr: this is crap.
+enum ActionType {
+  noaction = 0,
+  execute,
+  iconify,
+  raiseWindow,
+  lowerWindow,
+  closeWindow,
+  changeDesktop,
+  shade,
+  moveWindowUp,
+  moveWindowDown,
+  moveLeft,
+  moveRight,
+  nextWindow,
+  prevWindow,
+
+  stringChain, 
+  keyChain,
+  numberChain,
+
+  cancel
+};
+

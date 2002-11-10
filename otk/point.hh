8693dd95a2cdd45a5b0668ea2df1e4d20e238ee8
Dana Jansens <danakj@orodu.net>
2002-11-10 10:52:56 +0000

    document the Point class

--- otk/point.hh
+++ otk/point.hh
@@ -3,22 +3,33 @@
 #define __point_hh
 
 /*! @file point.hh
+  @brief The Point class contains an x/y pair
 */
 
 namespace otk {
 
+//! The Point class is an x/y coordinate or size pair
 class Point {
 private:
-  int _x, _y;
+  //! The x value
+  int _x;
+  //! The y value
+ int _y;
 
 public:
+  //! Constructs a new Point with 0,0 values
   Point() : _x(0), _y(0) {}
+  //! Constructs a new Point with given values
   Point(int x, int y) : _x(x), _y(y) {}
 
+  //! Changes the x value to the new value specified
   void setX(int x) { _x = x; }
+  //! Returns the x value
   void x() const { return _x; }
 
+  //! Changes the y value to the new value specified
   void setY(int x) { _x = x; }
+  //! Returns the y value
   void y() const { return _x; }
 };
 

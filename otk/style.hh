6852efaa9e4c5915aba5f9895a8f09cd9593083e
Marius Nita <marius@cs.pdx.edu>
2002-11-11 03:45:46 +0000

    added accessors for elements

--- otk/style.hh
+++ otk/style.hh
@@ -16,6 +16,10 @@ namespace otk {
 class Style {
 public:
 
+  enum Type { ButtonFocus, ButtonUnfocus, TitleFocus, TitleUnfocus,
+              LabelFocus, LabelUnfocus, HandleFocus, HandleUnfocus,
+              GripFocus, GripUnfocus };
+
   enum TextJustify { LeftJustify = 1, RightJustify, CenterJustify };
   enum BulletType { RoundBullet = 1, TriangleBullet, SquareBullet, NoBullet };
 
@@ -47,6 +51,7 @@ public:
 
   PixmapMask close_button, max_button, icon_button, stick_button;
   TextJustify justify;
+  BulletType bullet_type;
 
   unsigned int handle_width, bevel_width, frame_width, border_width;
 
@@ -69,18 +74,49 @@ public:
                         const Configuration &style);
   
   BTexture readDatabaseTexture(const std::string &rname,
-                                    const std::string &default_color,
-                                    const Configuration &style, 
-                                    bool allowNoTexture = false);
+                               const std::string &default_color,
+                               const Configuration &style, 
+                               bool allowNoTexture = false);
 
   BColor readDatabaseColor(const std::string &rname,
-                                const std::string &default_color,
-                                const Configuration &style);
+                           const std::string &default_color,
+                           const Configuration &style);
 
   BFont *readDatabaseFont(const std::string &rbasename,
-                               const Configuration &style);
+                          const Configuration &style);
 
   void load(Configuration &);
+
+  inline BColor *getBorderColor(void) { return &border_color; }
+
+  inline BColor *getTextFocus(void) { return &l_text_focus; }
+  inline BColor *getTextUnfocus(void) { return &l_text_unfocus; }
+
+  inline BColor *getButtonPicFocus(void) { return &b_pic_focus; }
+  inline BColor *getButtonPicUnfocus(void) { return &b_pic_unfocus; }
+
+  inline BTexture *getFrameFocus(void) { return &f_focus; }
+  inline BTexture *getFrameUnfocus(void) { return &f_unfocus; }
+
+  inline BTexture *getTitleFocus(void) { return &t_focus; }
+  inline BTexture *getTitleUnfocus(void) { return &t_unfocus; }
+
+  inline BTexture *getLabelFocus(void) { return &l_focus; }
+  inline BTexture *getLabelUnfocus(void) { return &l_unfocus; }
+
+  inline BTexture *getHandleFocus(void) { return &h_focus; }
+  inline BTexture *getHandleUnfocus(void) { return &h_unfocus; }
+
+  inline BTexture *getButtonFocus(void) { return &b_focus; }
+  inline BTexture *getButtonUnfocus(void) { return &b_unfocus; }
+
+  inline BTexture *getButtonPressedFocus(void)
+  { return &b_pressed_focus; }
+  inline BTexture *getButtonPressedUnfocus(void)
+  { return &b_pressed_unfocus; }
+
+  inline BTexture *getGripFocus(void) { return &g_focus; }
+  inline BTexture *getGripUnfocus(void) { return &g_unfocus; }
 
   inline unsigned int getHandleWidth(void) const { return handle_width; }
   inline unsigned int getBevelWidth(void) const { return bevel_width; }
@@ -88,6 +124,10 @@ public:
   inline unsigned int getBorderWidth(void) const { return border_width; }
 
   inline const BFont &getFont() const { return *font; }
+  inline bool hasAAFonts(void) const { return aa_fonts; }
+
+  inline TextJustify textJustify(void) { return justify; }
+  inline BulletType bulletType(void) { return bullet_type; }
 
   inline const BColor &getBorderColor() const { return border_color; }
 
@@ -96,6 +136,7 @@ public:
 
   inline void setImageControl(BImageControl *c) { image_control = c; }
   inline void setScreenNumber(unsigned int scr) { screen_number = scr; }
+  inline unsigned int getScreen(void) { return screen_number; }
 
   // XXX add inline accessors for the rest of the bummy
 };

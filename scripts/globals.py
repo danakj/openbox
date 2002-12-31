30a962664b73f72aeba1b9b184c00cf6b02ebf22
Dana Jansens <danakj@orodu.net>
2002-12-31 08:50:13 +0000

    add comments/headers

--- scripts/globals.py
+++ scripts/globals.py
@@ -1,15 +1,19 @@
-# openbox - pointer to the current Openbox instance
-openbox = Openbox_instance()
-
-# screen - list of all screens in the current openbox instance
-screen = []
-for i in range(Openbox_screenCount(openbox)):
-    screen.append(Openbox_screen(openbox, i))
+#############################################################################
+### Options that can be defined on startup that affect the behavior of    ###
+### openbox.                                                              ###
+#############################################################################
 
 # client_buttons - a list of the modifier(s) and buttons which are grabbed on
 #                  client windows (for interactive move/resize, etc)
 #  examples: "A-2", "C-A-2", "W-1"
 client_buttons = ["A-1", "A-2", "A-3"]
+
+
+
+#############################################################################
+### Options that can be modified by the user to change the default hooks' ###
+### behaviors.                                                            ###
+#############################################################################
 
 # click_focus - true if clicking in a client will cause it to focus in the
 #               default hook functions
@@ -20,5 +24,21 @@ enter_focus = 1
 # leave_unfocus - true if leaving a client window will cause it to unfocus in
 #                 the default hook functions
 leave_unfocus = 1
+
+
+
+#############################################################################
+### Variables defined for other scripts to use.                           ###
+#############################################################################
+
+# openbox - pointer to the current Openbox instance
+openbox = Openbox_instance()
+
+# screen - list of all screens in the current openbox instance
+screen = []
+for i in range(Openbox_screenCount(openbox)):
+    screen.append(Openbox_screen(openbox, i))
+
+
 
 print "Loaded globals.py"

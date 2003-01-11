87688e399ce4bbc11494772eb6b73813dca9e46d
Dana Jansens <danakj@orodu.net>
2003-01-11 20:51:00 +0000

    use new alias for mbuttons

--- scripts/defaults.py
+++ scripts/defaults.py
@@ -10,7 +10,7 @@ setup_fallback_focus()
 ebind(EventPlaceWindow, placewindows_random)
 
 # run xterm from root clicks
-mbind("1", MC_Root, MouseClick, lambda(d): execute("xterm"))
+mbind("Left", MC_Root, MouseClick, lambda(d): execute("xterm"))
 
 kbind(["A-F4"], KC_All, close)
 

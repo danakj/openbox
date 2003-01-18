23940f9e034af023af610c56e915a6044ea436ec
Dana Jansens <danakj@orodu.net>
2003-01-18 04:56:30 +0000

    add a new texture and style class

---
+++ otk/renderstyle.cc
@@ -0,0 +1,11 @@
+// -*- mode: C++; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+
+#ifdef    HAVE_CONFIG_H
+#  include "../config.h"
+#endif // HAVE_CONFIG_H
+
+#include "renderstyle.hh"
+
+namespace otk {
+
+}
---
+++ otk/renderstyle.hh
@@ -0,0 +1,14 @@
+// -*- mode: C++; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+#ifndef __renderstyle_hh
+#define __renderstyle_hh
+
+namespace otk {
+
+#include "rendertexture.hh"
+
+class RenderStyle {
+};
+
+}
+
+#endif // __rendertexture_hh

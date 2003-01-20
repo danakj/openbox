24e20d5ed6f07f61bbc8ee2bdeac377ca2373bdb
Dana Jansens <danakj@orodu.net>
2003-01-20 17:14:32 +0000

    add most of our style elements

--- otk/renderstyle.hh
+++ otk/renderstyle.hh
@@ -7,6 +7,47 @@
 namespace otk {
 
 class RenderStyle {
+public:
+  enum TextJustify {
+    LeftJustify,
+    RightJustify,
+    CenterJustify
+  };
+
+private:
+  int _screen;
+  
+  RenderColor *_text_focus_color;
+  RenderColor *_text_unfocus_color;
+
+  RenderColor *_frame_border_color;
+  int _frame_border_wirth;
+  RenderColor *_client_border_color_focus; 
+  RenderColor *_client_border_color_unfocus;
+  int _client_border_width;
+ 
+  RenderTexture *_titlebar_focus;
+  RenderTexture *_titlebar_unfocus;
+
+  RenderTexture *_label_focus;
+  RenderTexture *_label_unfocus;
+
+  RenderTexture *_handle_focus;
+  RenderTexture *_handle_unfocus;
+
+  RenderTexture *_button_unpress_focus;
+  RenderTexture *_button_unpress_unfocus;
+  RenderTexture *_button_press_focus;
+  RenderTexture *_button_press_unfocus;
+
+  RenderTexture *_grip_focus;
+  RenderTexture *_grip_unfocus;
+
+  Font *_label_font;
+  TextJustify _label_justify;
+
+  int _handle_width;
+  int _bevel_width;
 };
 
 }

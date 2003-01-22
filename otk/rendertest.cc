fabb6e8c1faa8339730cc7f56f84eb0a7e8b6c4f
Derek Foreman <manmower@gmail.com>
2003-01-22 02:50:29 +0000

    testing a gradient texture now

--- otk/rendertest.cc
+++ otk/rendertest.cc
@@ -13,16 +13,18 @@ int main(int argc, char **argv)
   otk::AppWidget foo(&app);
   foo.resize(600, 500);
 
-  otk::RenderColor color(0, 0, 0xff, 0xff);
+  otk::RenderColor color(0, 0x96, 0xba, 0x86);
+  otk::RenderColor color2(0, 0x5a, 0x72, 0x4c);
   otk::RenderColor colord(0, 0, 0, 0);
   otk::RenderColor colorl(0, 0xff, 0xff, 0xff);
   otk::RenderTexture tex(false,
 			 otk::RenderTexture::Raised,
 			 otk::RenderTexture::Bevel1,
 			 false,
-			 otk::RenderTexture::Solid,
+			 otk::RenderTexture::Vertical,
 			 false,
 			 &color,
+                         &color2,
 			 &colord,
 			 &colorl,
 			 0,

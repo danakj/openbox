77882b46dbeb1f626f052e2eb3cb200d44f3e3ff
Dana Jansens <danakj@orodu.net>
2003-01-22 23:11:30 +0000

    break when no more widgets showing

--- otk/application.cc
+++ otk/application.cc
@@ -63,6 +63,8 @@ void Application::run(void)
 
   while (_appwidget_count > 0) {
     dispatchEvents();
+    if (_appwidget_count <= 0)
+      break;
     Timer::dispatchTimers(); // fire pending events
   }
 }

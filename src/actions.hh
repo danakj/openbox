3682df8ed27e96067a171b71cbf58d5a00ca813b
Dana Jansens <danakj@orodu.net>
2003-01-24 19:21:07 +0000

    include widgetbase not widget

--- src/actions.hh
+++ src/actions.hh
@@ -6,7 +6,7 @@
   @brief The action interface for user-available actions
 */
 
-#include "widget.hh"
+#include "widgetbase.hh"
 #include "otk/point.hh"
 #include "otk/rect.hh"
 #include "otk/eventhandler.hh"

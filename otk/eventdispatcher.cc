e767dd79c72e97b4664a6b7f4bad5ee7cab7b475
Dana Jansens <danakj@orodu.net>
2003-01-29 00:47:32 +0000

    only ignore NotifyGrab again

--- otk/eventdispatcher.cc
+++ otk/eventdispatcher.cc
@@ -105,7 +105,7 @@ void EventDispatcher::dispatchFocus(const XEvent &e)
 //         (e.xfocus.type == FocusIn ? "IN" : "OUT"),
 //         e.xfocus.detail, e.xfocus.window);
   // ignore focus changes from grabs
-  if (e.xfocus.mode != NotifyNormal) //|| e.xfocus.mode == NotifyUngrab ||
+  if (e.xfocus.mode == NotifyGrab) //|| e.xfocus.mode == NotifyUngrab ||
       // From Metacity, from WindowMaker, ignore all funky pointer root events
       // its commented out cuz I don't think we need this at all. If problems
       // arise we can look into it

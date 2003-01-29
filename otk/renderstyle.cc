edcd9726300534de19c767f88022ae0fcc7a7b75
Dana Jansens <danakj@orodu.net>
2003-01-29 23:16:11 +0000

    add merry's style as an option

--- otk/renderstyle.cc
+++ otk/renderstyle.cc
@@ -14,6 +14,11 @@ RenderStyle::RenderStyle(int screen, const std::string &stylefile)
   : _screen(screen),
     _file(stylefile)
 {
+// pick one..
+//#define FIERON
+#define MERRY
+
+#ifdef FIERON
   _root_color = new RenderColor(_screen, 0x272a2f);
   
   _text_color_focus = new RenderColor(_screen, 0x272a2f);
@@ -218,6 +223,218 @@ RenderStyle::RenderStyle(int screen, const std::string &stylefile)
 
   _bevel_width = 1;
   _handle_width = 4;
+#else
+#  ifdef MERRY
+  _root_color = new RenderColor(_screen, 0x7b756a);
+  
+  _text_color_focus = new RenderColor(_screen, 0xffffff);
+  _text_color_unfocus = new RenderColor(_screen, 0xffffff);
+
+  _button_color_focus = new RenderColor(_screen, 0x222222);
+  _button_color_unfocus = new RenderColor(_screen, 0x333333);
+
+  _frame_border_color = new RenderColor(_screen, 0x222222);
+  _frame_border_width = 1;
+
+  _client_border_color_focus = new RenderColor(_screen, 0x858687);
+  _client_border_color_unfocus = new RenderColor(_screen, 0x555657);
+  _client_border_width = 0;
+
+  _titlebar_focus = new RenderTexture(_screen,
+                                      false,
+                                      RenderTexture::Flat,
+                                      RenderTexture::Bevel1,
+                                      false,
+                                      RenderTexture::Solid,
+                                      false,
+                                      0xe6e6e6,
+                                      0xe6e6e6,  
+				      0x0,
+                                      0x0);
+  _titlebar_unfocus = new RenderTexture(_screen,
+                                        false,
+                                        RenderTexture::Flat,
+                                        RenderTexture::Bevel1,
+                                        false,
+                                        RenderTexture::Solid,
+                                        false,
+                                        0xe6e6e6,
+                                        0xe6e6e6,
+                                        0x0,
+                                        0x0);
+
+  _label_focus = new RenderTexture(_screen,
+                                   false,
+                                   RenderTexture::Flat,
+                                   RenderTexture::Bevel1,
+                                   true,
+                                   RenderTexture::Solid,
+                                   false,
+                                   //0x6a6973,
+                                   //0x6a6973,
+                                   0x4c59a6,
+ 			           0x4c59a6,
+				   0x222222,
+                                   0x0);
+  _label_unfocus = new RenderTexture(_screen,
+                          		false,
+                                   RenderTexture::Flat,
+                                   RenderTexture::Bevel1,
+                                   true,
+                                   RenderTexture::Solid,
+                                   false,
+                                   0xcdcac7,
+                                   0xcdcac7,
+				0xb0ada9,
+                                   0x0);
+
+
+  _handle_focus = new RenderTexture(_screen,
+                                    false,
+                                    RenderTexture::Flat,
+                                    RenderTexture::Bevel1,
+                                    false,
+                                    RenderTexture::Solid,
+                                    false,
+                                    0xe6e6e6,
+                                        0xe6e6e6,
+                                    0x0,
+                                    0x0);
+  _handle_unfocus = new RenderTexture(_screen,
+                                      false,
+                                      RenderTexture::Flat,
+                                      RenderTexture::Bevel1,
+                                      false,
+                                      RenderTexture::Solid,
+                                      false,
+                                     0xe6e6e6,
+                                        0xe6e6e6,
+                                      0x0,
+                                      0x0);
+
+  
+  _button_unpress_focus = new RenderTexture(_screen,
+                                            false,
+                                            RenderTexture::Flat,
+                                            RenderTexture::Bevel1,
+                                            false,
+                                            RenderTexture::Solid,
+                                            false,
+                                            0xe6e6e6,
+                                        0xe6e6e6,
+                                            0x0,
+                                            0x0);
+  _button_unpress_unfocus = new RenderTexture(_screen,
+                                              false,
+                                              RenderTexture::Flat,
+                                              RenderTexture::Bevel1,
+                                              false,
+                                              RenderTexture::Solid,
+                                              false,
+                                             0xe6e6e6,
+                                        0xe6e6e6,
+                                              0x0,
+                                              0x0);
+
+  _button_press_focus = new RenderTexture(_screen,
+                                          false,
+                                            RenderTexture::Sunken,
+                                            RenderTexture::Bevel1,
+                                          false,
+                                          RenderTexture::Solid,
+                                          false,
+                                          0xe6e6e6,
+                                          0xe6e6e6,
+                                          0x0,
+                                          0x0);
+  _button_press_unfocus = new RenderTexture(_screen,
+                                            false,
+                                              RenderTexture::Sunken,
+                                            RenderTexture::Bevel1,
+                                            false,
+                                            RenderTexture::Solid,
+                                            false,
+                                              0xe6e6e6,
+                                        0xe6e6e6,
+                                            0x0,
+                                            0x0);
+
+  _grip_focus = new RenderTexture(_screen,
+                                  false,
+                                  RenderTexture::Flat,
+                                  RenderTexture::Bevel1,
+                                  false,
+                                  RenderTexture::Solid,
+                                  false,
+                                    0xe6e6e6,
+                                        0xe6e6e6,
+                                  0x0,
+                                  0x0);
+  _grip_unfocus = new RenderTexture(_screen,
+                                    false,
+                                    RenderTexture::Flat,
+                                    RenderTexture::Bevel1,
+                                    false,
+                                    RenderTexture::Solid,
+                                    false,
+                                      0xe6e6e6,
+                                        0xe6e6e6,
+                                    0x0,
+                                    0x0);
+
+  _label_font = new Font(_screen, "Arial,Sans-10", true, 1, 0x3e);
+  _label_justify = CenterJustify;
+
+  _max_mask = new PixmapMask();
+  _max_mask->w = _max_mask->h = 7;
+  {
+    //char data[] = { 0x7e, 0xff, 0xc3, 0xc3, 0xc3, 0xc3, 0xff, 0x7e };
+    char data []  = {0x7c, 0x44, 0x47, 0x47, 0x7f, 0x1f, 0x1f  };
+    _max_mask->mask =
+      XCreateBitmapFromData(**display,
+                            display->screenInfo(_screen)->rootWindow(),
+                            data, 7, 7);
+  }
+
+  _icon_mask = new PixmapMask();
+  _icon_mask->w = _icon_mask->h = 7;
+  {
+    //char data[] = { 0x00, 0x00, 0xc3, 0xe7, 0x7e, 0x3c, 0x18, 0x00 };
+    char data[] = {0x00, 0x00, 0x00, 0x00, 0x3e, 0x3e, 0x3e };
+    _icon_mask->mask =
+      XCreateBitmapFromData(**display,
+                            display->screenInfo(_screen)->rootWindow(),
+                            data, 7, 7);
+  }
+  
+  _alldesk_mask = new PixmapMask();
+  _alldesk_mask->w = _alldesk_mask->h = 7;
+  {
+    //char data[] = { 0x00, 0x00, 0x18, 0x3c, 0x3c, 0x18, 0x00, 0x00 };
+    char data[] = {0x00, 0x36, 0x36, 0x00, 0x36, 0x36, 0x00 };
+    _alldesk_mask->mask =
+      XCreateBitmapFromData(**display,
+                            display->screenInfo(_screen)->rootWindow(),
+                            data, 7, 7);
+  }
+  
+  _close_mask = new PixmapMask();
+  _close_mask->w = _close_mask->h = 7;
+  {
+    //char data[] = { 0xc3, 0xe7, 0x7e, 0x3c, 0x3c, 0x7e, 0xe7, 0xc3 };
+    char data[] = {  0x22, 0x77, 0x3e, 0x1c, 0x3e, 0x77, 0x22 };
+    _close_mask->mask =
+      XCreateBitmapFromData(**display,
+                            display->screenInfo(_screen)->rootWindow(),
+                            data, 7, 7);
+  }
+
+  _bevel_width = 1;
+  _handle_width = 3;
+#  else
+#    error 1
+#  endif
+#endif
 }
 
 RenderStyle::~RenderStyle()

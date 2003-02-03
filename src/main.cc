f5904cdaaafa95e8ab6ffde212dfc6b7478b4e32
Dana Jansens <danakj@orodu.net>
2003-02-03 04:11:58 +0000

    delete the openbox class, to shut things down properly!

--- src/main.cc
+++ src/main.cc
@@ -36,11 +36,14 @@ int main(int argc, char **argv) {
   bind_textdomain_codeset(PACKAGE, "UTF-8");
   textdomain(PACKAGE);
 
-  ob::Openbox openbox(argc, argv);
-  openbox.eventLoop();
+  ob::Openbox *openbox = new ob::Openbox(argc, argv);
+  openbox->eventLoop();
 
-  if (openbox.doRestart()) {
-    const std::string &prog = openbox.restartProgram();
+  if (openbox->doRestart()) {
+    std::string prog = openbox->restartProgram();
+
+    delete openbox; // shutdown the current one!
+    
     if (!prog.empty()) {
       execl("/bin/sh", "/bin/sh", "-c", prog.c_str(), NULL); 
       perror(prog.c_str());
@@ -50,4 +53,6 @@ int main(int argc, char **argv) {
     execvp(argv[0], argv);
     execvp(otk::basename(argv[0]).c_str(), argv);
   }
+
+  delete openbox; // shutdown
 }

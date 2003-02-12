9faca110acaffe21fd9f14cef807bc7aeb289a2a
Dana Jansens <danakj@orodu.net>
2003-02-12 00:34:22 +0000

    kill all the old commented vars in rendercontrol.hh add some preliminary vars in pseudorendercontrol.hh

--- otk/pseudorendercontrol.hh
+++ otk/pseudorendercontrol.hh
@@ -8,7 +8,19 @@ namespace otk {
 
 class PseudoRenderControl : public RenderControl {
 private:
-  // add some vars!!!
+  // color tables, meaning, 256 (possibly) different shades of each color,
+  // based on the number of bits there are available for each color in the
+  // visual
+  unsigned char _red_color_table[256];
+  unsigned char _green_color_table[256];
+  unsigned char _blue_color_table[256];
+
+  int _cpc; // colors-per-channel: must be a value between [2,6]
+  int _bpp; // bits-per-pixel
+
+  // These are only used for !TrueColor visuals
+  XColor *_colors;
+  int _ncolors;
   
   virtual void reduceDepth(Surface &sf, XImage *im) const;
   
--- otk/rendercontrol.hh
+++ otk/rendercontrol.hh
@@ -22,46 +22,7 @@ class RenderControl {
 protected:
   int _screen;
 
-  /*
-  // color tables, meaning, 256 (possibly) different shades of each color,
-  // based on the number of bits there are available for each color in the
-  // visual
-  unsigned char _red_color_table[256];
-  unsigned char _green_color_table[256];
-  unsigned char _blue_color_table[256];
-  */
-
-/*
-  Bool _dither;
-
-  int _cpc; // colors-per-channel: must be a value between [2,6]
-  int _bpp; // bits-per-pixel
-
-  unsigned int *_grad_xbuffer;
-  unsigned int *_grad_ybuffer;
-  unsigned int _grad_buffer_width;
-  unsigned int _grad_buffer_height;
-
-  unsigned long *_sqrt_table;
-
-  // These values are all determined based on a visual
-
-  int _red_bits;    // the number of bits (1-255) that each shade of color
-  int _green_bits;  // spans across. best case is 1, which gives 255 shades.
-  int _blue_bits;
-  unsigned char _red_color_table[256];
-  unsigned char _green_color_table[256];
-  unsigned char _blue_color_table[256];
-
-  // These are only used for TrueColor visuals
-  int _red_offset;  // the offset of each color in a color mask
-  int _green_offset;
-  int _blue_offset;
-
-  // These are only used for !TrueColor visuals
-  XColor *_colors;
-  int _ncolors;
-*/
+//  bool _dither;
 
   RenderControl(int screen);
 

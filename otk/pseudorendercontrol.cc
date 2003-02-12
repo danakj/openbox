dd89811570e9a8a651821a7d3b175244e3d61f01
Dana Jansens <danakj@orodu.net>
2003-02-12 06:14:25 +0000

    alloc colors.. i think!

--- otk/pseudorendercontrol.cc
+++ otk/pseudorendercontrol.cc
@@ -26,24 +26,79 @@ PseudoRenderControl::PseudoRenderControl(int screen)
   const ScreenInfo *info = display->screenInfo(_screen);
   int depth = info->depth();
 
-  _cpc = 4; // XXX THIS SHOULD BE A USER OPTION
-  _ncolors = _cpc * _cpc * _cpc;
+  _bpc = 2; // XXX THIS SHOULD BE A USER OPTION
+  assert(_bpc >= 1);
+  _ncolors = 1 << (_bpc * 3);
 
-  if (_cpc < 2 || _ncolors > 1 << depth) {
+  if (_ncolors > 1 << depth) {
     fprintf(stderr,
-            _("PseudoRenderControl: Invalid colormap size. Using maximum size
-available.\n"));
-    _cpc = 1 << (depth/3);
-    _ncolors = 1 << depth; // _cpc * _cpc * _cpc
+            _("PseudoRenderControl: Invalid colormap size. Resizing.\n"));
+    _bpc = 1 << (depth/3) >> 3;
+    _ncolors = 1 << (_bpc * 3);
   }
 
+  _cpc = 1 << _bpc;
+  
   if (!(_colors = new XColor[_ncolors])) {
     fprintf(stderr,
             _("PseudoRenderControl: error allocating colormap\n"));
     ::exit(1);
   }
 
-  
+  // build a color cube
+  for (int n = _ncolors - 1,
+         r = (1 << (_bpc + 1)) -1, i = 0; i < _cpc; r >>= 1, ++i)
+    for (int g = (1 << (_bpc + 1)) -1, j = 0; j < _cpc; g >>= 1, ++j)
+      for (int b = (1 << (_bpc + 1)) -1, k = 0; k < _cpc; b >>= 1, ++k, --n) {
+        _colors[n].red = r | r << 8;
+        _colors[n].green = g | g << 8;
+        _colors[n].blue = b | b << 8;
+        _colors[n].flags = DoRed|DoGreen|DoBlue; // used to track allocation
+      }
+
+  // allocate the colors
+  for (int i = 0; i < _ncolors; i++)
+    if (!XAllocColor(**display, info->colormap(), &_colors[i]))
+      _colors[i].flags = 0; // mark it as unallocated
+
+  // try allocate any colors that failed allocation above
+
+  // get the allocated values from the X server (only the first 256 XXX why!?)
+  XColor icolors[256];
+  int incolors = (((1 << depth) > 256) ? 256 : (1 << depth));
+  for (int i = 0; i < incolors; i++)
+    icolors[i].pixel = i;
+  XQueryColors(**display, info->colormap(), icolors, incolors);
+
+  // try match unallocated ones
+  for (int i = 0; i < _ncolors; i++) {
+    if (!_colors[i].flags) { // if it wasn't allocated...
+      unsigned long closest = 0xffffffff, close = 0;
+      for (int ii = 0; ii < incolors; ii++) {
+        // find deviations
+        int r = (_colors[i].red - icolors[ii].red) & 0xff;
+        int g = (_colors[i].green - icolors[ii].green) & 0xff;
+        int b = (_colors[i].blue - icolors[ii].blue) & 0xff;
+        // find a weighted absolute deviation
+        unsigned long dev = (r * r) + (g * g) + (b * b);
+
+        if (dev < closest) {
+          closest = dev;
+          close = ii;
+        }
+      }
+
+      _colors[i].red = icolors[close].red;
+      _colors[i].green = icolors[close].green;
+      _colors[i].blue = icolors[close].blue;
+
+      // try alloc this closest color, it had better succeed!
+      if (XAllocColor(**display, info->colormap(), &_colors[i]))
+        _colors[i].flags = DoRed|DoGreen|DoBlue; // mark as alloced
+      else
+        assert(false); // wtf has gone wrong, its already alloced for chissake!
+    }
+  }
 }
 
 PseudoRenderControl::~PseudoRenderControl()

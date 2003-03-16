ea8d450ed30c96c1a5dd627740ec4533eeae2a09
Derek Foreman <manmower@gmail.com>
2003-03-16 23:34:45 +0000

    add font_draw move font def'n to render.h

--- render/font.h
+++ render/font.h
@@ -1,10 +1,7 @@
 #ifndef __font_h
 #define __font_h
 #include <X11/Xft/Xft.h>
-
-typedef struct {
-    XftFont *xftfont;
-} ObFont;
+#include "render.h"
 
 void font_startup(void);
 ObFont *font_open(char *fontstring);
@@ -12,4 +9,5 @@ void font_close(ObFont *f);
 int font_measure_string(ObFont *f, const char *str, int shadow, int offset);
 int font_height(ObFont *f, int shadow, int offset);
 int font_max_char_width(ObFont *f);
+void font_draw(XftDraw *d, TextureText *t);
 #endif /* __font_h */

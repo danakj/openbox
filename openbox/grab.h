bcca5bb967f792e6dd9a1031e4bdcbef13be754d
Dana Jansens <danakj@orodu.net>
2003-03-19 18:55:56 +0000

    add funcs to grab buttons and keys

--- openbox/grab.h
+++ openbox/grab.h
@@ -11,4 +11,12 @@ void grab_keyboard(gboolean grab);
 void grab_pointer(gboolean grab, Cursor cur);
 void grab_server(gboolean grab);
 
+void grab_button(guint button, guint state, Window win, guint mask,
+                 int pointer_mode);
+void ungrab_button(guint button, guint state, Window win);
+
+void grab_key(guint keycode, guint state, int keyboard_mode);
+
+void ungrab_all_keys();
+
 #endif

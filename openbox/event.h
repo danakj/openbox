080b36cfc21916534c375e478cd36e5623512d82
Dana Jansens <danakj@orodu.net>
2003-03-19 18:55:36 +0000

    export the NumLock and ScrollLock masks

--- openbox/event.h
+++ openbox/event.h
@@ -4,6 +4,11 @@
 /*! Time at which the last event with a timestamp occured. */
 extern Time event_lasttime;
 
+/*! The value of the mask for the NumLock modifier */
+extern unsigned int NumLockMask;
+/*! The value of the mask for the ScrollLock modifier */
+extern unsigned int ScrollLockMask;
+
 void event_startup();
 void event_shutdown();
 

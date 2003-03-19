a75829699ba5f7e9e6746ae68a04aec38406433a
Dana Jansens <danakj@orodu.net>
2003-03-19 07:26:05 +0000

    kill the const

--- render/font.h
+++ render/font.h
@@ -6,7 +6,7 @@
 void font_startup(void);
 ObFont *font_open(char *fontstring);
 void font_close(ObFont *f);
-int font_measure_string(ObFont *f, const char *str, int shadow, int offset);
+int font_measure_string(ObFont *f, char *str, int shadow, int offset);
 int font_height(ObFont *f, int shadow, int offset);
 int font_max_char_width(ObFont *f);
 void font_draw(XftDraw *d, TextureText *t, int x, int y, int w, int h);

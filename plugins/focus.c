bb7427422cc07cb9b31ce643b9bde75454f8d844
Dana Jansens <danakj@orodu.net>
2003-03-20 22:21:13 +0000

    skip all enter events for now.. need a clever tactic for this..

--- plugins/focus.c
+++ plugins/focus.c
@@ -6,39 +6,14 @@
 #include "../kernel/stacking.h"
 #include "../kernel/openbox.h"
 
+/* config options */
+static gboolean follow_mouse = TRUE;
+static gboolean warp_on_desk_switch = TRUE;
+static gboolean focus_new = FALSE;
+
 static int skip_enter = 0;
 
-static void focus_fallback(gboolean warp)
-{
-    GList *it;
-
-    for (it = focus_order[screen_desktop]; it != NULL; it = it->next)
-        if (client_focus(it->data)) {
-            if (warp) { /* XXX make this configurable */
-                XEvent e;
-                Client *c = it->data;
-
-                /* skip the next enter event from the desktop switch so focus
-                   doesn't skip briefly to what was under the pointer */
-                if (XCheckTypedEvent(ob_display, EnterNotify, &e)) {
-                    XPutBackEvent(ob_display, &e);
-                    /* XXX WERE NOT SKIPPING THEM ALL@&*)! */
-                    g_message("Skip");
-                    ++skip_enter;
-                }
-
-                /* I have to do this warp twice! Otherwise windows dont get
-                   Enter/Leave events when i warp on a desktop switch! */
-                XWarpPointer(ob_display, None, c->window, 0, 0, 0, 0,
-                             c->area.width / 2, c->area.height / 2);
-                XWarpPointer(ob_display, None, c->window, 0, 0, 0, 0,
-                             c->area.width / 2, c->area.height / 2);
-            }
-            break;
-        }
-}
-
-static void focus_under_pointer()
+static gboolean focus_under_pointer()
 {
     Window w;
     int i, x, y;
@@ -55,43 +30,69 @@ static void focus_under_pointer()
         }
         if (it != NULL) {
             client_focus(it->data);
-            return;
+            return TRUE;
         }
     }
-    focus_fallback(FALSE);
+    return FALSE;
+}
+
+static void focus_fallback(gboolean switching_desks)
+{
+    GList *it;
+
+    for (it = focus_order[screen_desktop]; it != NULL; it = it->next)
+        if (client_normal(it->data) && client_focus(it->data)) {
+            if (switching_desks && warp_on_desk_switch) {
+                XEvent e;
+                Client *c = it->data;
+
+                /* skip the next enter event from the desktop switch so focus
+                   doesn't skip briefly to what was under the pointer */
+                while (XCheckTypedEvent(ob_display, EnterNotify, &e));
+/*
+                    XPutBackEvent(ob_display, &e);
+                    /\* XXX WERE NOT SKIPPING THEM ALL@&*)! *\/
+                    g_message("Skip");
+                    ++skip_enter;
+                }
+*/
+
+                /* I have to do this warp twice! Otherwise windows dont get
+                   Enter/Leave events when i warp on a desktop switch! */
+                XWarpPointer(ob_display, None, c->window, 0, 0, 0, 0,
+                             c->area.width / 2, c->area.height / 2);
+                XWarpPointer(ob_display, None, c->window, 0, 0, 0, 0,
+                             c->area.width / 2, c->area.height / 2);
+            }
+            break;
+        }
 }
 
 static void events(ObEvent *e, void *foo)
 {
     switch (e->type) {
     case Event_Client_Mapped:
-        /* focus new normal windows */
-        if (client_normal(e->data.c.client))
+        if (focus_new && client_normal(e->data.c.client))
             client_focus(e->data.c.client);
         break;
 
     case Event_Client_Unmapped:
         if (ob_state == State_Exiting) break;
 
-        if (e->data.c.client->focused) {
-            /* if sloppy focus... */
-            focus_under_pointer();
-
-            /* otherwise... */
-            /*
-              /\* nothing is left with focus! *\/
-              if (focus_client == NULL) 
-              /\* focus the next available target *\/
-              focus_fallback(screen_desktop, FALSE);
-            */
-        }
+        if (e->data.c.client->focused)
+            if (!follow_mouse || !focus_under_pointer())
+                focus_fallback(FALSE);
         break;
 
-    case Event_Ob_Desktop:
+    case Event_Client_Desktop:
         /* focus the next available target if moving from the current
            desktop. */
-        if ((unsigned)e->data.o.num[1] == screen_desktop)
-            focus_fallback(TRUE);
+        if ((unsigned)e->data.c.num[1] == screen_desktop)
+            if (!follow_mouse || !focus_under_pointer())
+                focus_fallback(FALSE);
+
+    case Event_Ob_Desktop:
+        focus_fallback(TRUE);
         break;
 
     case Event_X_EnterNotify:

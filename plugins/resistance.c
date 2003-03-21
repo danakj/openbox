1418e9edc4926a0061fa3c2c8d036a33acd55491
Dana Jansens <danakj@orodu.net>
2003-03-21 09:33:41 +0000

    dont snap to self

--- plugins/resistance.c
+++ plugins/resistance.c
@@ -38,6 +38,7 @@ static void resist(Client *c, int *x, int *y)
             int tl, tt, tr, tb; /* 1 past the target's edges on each side */
 
             target = it->data;
+            if (target == c) continue; /* don't snap to self */
 
             tl = target->frame->area.x - 1;
             tt = target->frame->area.y - 1;

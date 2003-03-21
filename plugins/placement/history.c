0edc14a4f92e34f5edcdb00898013206142ca71d
Dana Jansens <danakj@orodu.net>
2003-03-21 07:54:48 +0000

    set up the history place stuff's files

---
+++ plugins/placement/history.c
@@ -0,0 +1,16 @@
+#include "../../kernel/frame.h"
+#include "../../kernel/client.h"
+#include <glib.h>
+
+void history_startup()
+{
+}
+
+void history_shutdown()
+{
+}
+
+gboolean place_history(Client *c)
+{
+    return FALSE;
+}

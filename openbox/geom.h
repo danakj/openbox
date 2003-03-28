66969a6ba5d55d74189a8cad68b67cf6cc16dd79
Dana Jansens <danakj@orodu.net>
2003-03-28 04:32:37 +0000

    make better?

--- openbox/geom.h
+++ openbox/geom.h
@@ -6,14 +6,14 @@ typedef struct Point {
     int y;
 } Point;
 
-#define POINT_SET(pt, nx, ny) pt.x = nx, pt.y = ny
+#define POINT_SET(pt, nx, ny) pt.x = (nx), pt.y = (ny)
 
 typedef struct Size {
     int width;
     int height;
 } Size;
 
-#define SIZE_SET(sz, w, h) sz.width = w, sz.height = h
+#define SIZE_SET(sz, w, h) sz.width = (w), sz.height = (h)
 
 typedef struct Rect {
     int x;
@@ -23,11 +23,11 @@ typedef struct Rect {
 } Rect;
 
 #define RECT_SET_POINT(r, nx, ny) \
-    r.x = ny, r.y = ny
+    r.x = (ny), r.y = (ny)
 #define RECT_SET_SIZE(r, w, h) \
-    r.width = w, r.height = h
+    r.width = (w), r.height = (h)
 #define RECT_SET(r, nx, ny, w, h) \
-    r.x = nx, r.y = ny, r.width = w, r.height = h
+    r.x = (nx), r.y = (ny), r.width = (w), r.height = (h)
 
 #define RECT_EQUAL(r1, r2) (r1.x == r2.x && r1.y == r2.y && \
 			    r1.width == r2.width && r1.height == r2.height)
@@ -43,7 +43,7 @@ typedef struct Strut {
 } Strut;
 
 #define STRUT_SET(s, l, t, r, b) \
-    s.left = l, s.top = t, s.right = r, s.bottom = b
+    s.left = (l), s.top = (t), s.right = (r), s.bottom = (b)
 
 #define STRUT_ADD(s1, s2) \
     s1.left = MAX(s1.left, s2.left), s1.right = MAX(s1.right, s2.right), \

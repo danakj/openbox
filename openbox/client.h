64dac07d07f37ec965ccc0b5bc8c8c789247cff0
Dana Jansens <danakj@orodu.net>
2003-04-10 06:50:57 +0000

    no fuck that it was working fine

--- openbox/client.h
+++ openbox/client.h
@@ -11,7 +11,7 @@ struct Group;
 
 /* The value in client.transient_for indicating it is a transient for its
    group instead of for a single window */
-#define TRAN_GROUP ((void*)~0ul)
+#define TRAN_GROUP ((void*)~0l)
 
 /*! Holds an icon in ARGB format */
 typedef struct Icon {

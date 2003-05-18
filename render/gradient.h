d9bdc211920d12fe9964fbaca302121fa77d5118
Derek Foreman <manmower@gmail.com>
2003-05-18 19:23:04 +0000

    add stubs for pipecross and rectangle

--- render/gradient.h
+++ render/gradient.h
@@ -9,6 +9,8 @@ void gradient_horizontal(Surface *sf, int w, int h);
 void gradient_diagonal(Surface *sf, int w, int h);
 void gradient_crossdiagonal(Surface *sf, int w, int h);
 void gradient_pyramid(Surface *sf, int w, int h);
+void gradient_pipecross(Surface *sf, int w, int h);
+void gradient_rectangle(Surface *sf, int w, int h);
 void gradient_solid(Appearance *l, int x, int y, int w, int h);
 void highlight(pixel32 *x, pixel32 *y, gboolean raised);
 

927f99e4125743bbecf41b1aa7dbce9587fb6156
Dana Jansens <danakj@orodu.net>
2003-05-24 20:50:56 +0000

    not being developed anymore, maybe in the future...

--- cwmcc/cwmcc.h
+++
@@ -1,6 +0,0 @@
-#ifndef __cwmcc_cwmcc_h
-#define __cwmcc_cwmcc_h
-
-void cwmcc_startup(Display *d);
-
-#endif
--- cwmcc/cwmcc_internal.h
+++
@@ -1,8 +0,0 @@
-#ifndef __cwmcc_cwmcc_internal_h
-#define __cwmcc_cwmcc_internal_h
-
-#include <X11/Xlib.h>
-
-extern Display *cwmcc_display;
-
-#endif

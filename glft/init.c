e13df7cec358b97c3a7c4beeadfa21986f61a908
Dana Jansens <danakj@orodu.net>
2003-06-08 04:48:32 +0000

    register my glft extensions to the fontpattern

--- glft/init.c
+++ glft/init.c
@@ -8,7 +8,17 @@
 FcBool init_done = 0;
 FT_Library ft_lib;
 
+FcObjectType objs[] = {
+    { GLFT_SHADOW,        FcTypeBool    },
+    { GLFT_SHADOW_OFFSET, FcTypeInteger },
+    { GLFT_SHADOW_ALPHA,  FcTypeDouble  }
+};
+
+#define NUM_OBJS (sizeof(objs) / sizeof(objs[0]))
+
 FcBool GlftInit()
 {
-    return init_done = (FcInit() && !FT_Init_FreeType(&ft_lib));
+    if ((init_done = (FcInit() && !FT_Init_FreeType(&ft_lib))))
+        FcNameRegisterObjectTypes(objs, NUM_OBJS);
+    return init_done;
 }

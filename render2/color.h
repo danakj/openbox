d53b9ea8953f3b2b3ea3403a3cb3bdbcf83b816f
Derek Foreman <manmower@gmail.com>
2003-06-17 00:31:01 +0000

    add color inspect and default byte order stuff

--- render2/color.h
+++ render2/color.h
@@ -3,6 +3,9 @@
 
 #include "render.h"
 
+/*! Sets up color masks for grabbing pixmaps from the server*/
+void RrColorInspect(struct RrInstance *i);
+
 /*! Returns if an RrColor is non-opaque */
 #define RrColorHasAlpha(c) ((c).a < 0.999999999)
 
@@ -16,5 +19,18 @@
                ((c1)->b + (c2)->b) / 2.0, \
                ((c1)->a + (c2)->a) / 2.0)
 
+#if (G_BYTE_ORDER == G_BIG_ENDIAN)
+#define default_red_offset 0
+#define default_green_offset 8
+#define default_blue_offset 16
+#define default_alpha_offset 24
+#define render_endian MSBFirst  
+#else
+#define default_alpha_offset 24
+#define default_red_offset 16
+#define default_green_offset 8
+#define default_blue_offset 0
+#define render_endian LSBFirst
+#endif /* G_BYTE_ORDER == G_BIG_ENDIAN */
 
 #endif

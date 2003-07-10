06d3487d49791015902b322408ebb105ba82d9b7
Dana Jansens <danakj@orodu.net>
2003-07-10 07:17:05 +0000

    dont need client.h in this header

--- openbox/moveresize.h
+++ openbox/moveresize.h
@@ -1,17 +1,18 @@
 #ifndef __moveresize_h
 #define __moveresize_h
 
-#include "client.h"
-
 #include <glib.h>
 
+struct _ObClient;
+
 extern gboolean moveresize_in_progress;
-extern ObClient *moveresize_client;
+extern struct _ObClient *moveresize_client;
 
 void moveresize_startup();
 void moveresize_shutdown();
 
-void moveresize_start(ObClient *c, int x, int y, guint button, guint32 corner);
+void moveresize_start(struct _ObClient *c,
+                      int x, int y, guint button, guint32 corner);
 void moveresize_end(gboolean cancel);
 
 void moveresize_event(XEvent *e);

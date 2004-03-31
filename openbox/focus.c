269fbc0fe08070c3c4be64629d121220714ce977
Mikael Magnusson <mikachu@comhem.se>
2004-03-31 09:56:01 +0000

    show parents title in alt-tab popup for titleless transients

--- openbox/focus.c
+++ openbox/focus.c
@@ -356,14 +356,13 @@ static void popup_cycle(ObClient *c, gboolean show)
         while (p->transient_for && p->transient_for != OB_TRAN_GROUP)
             p = p->transient_for;
 
-/*
-        if (p != c)
-            title = g_strconcat((c->iconic ? c->icon_title : c->title),
+        if (p != c && !strcmp("", (c->iconic ? c->icon_title : c->title)))
+            title = g_strdup(p->iconic ? p->icon_title : p->title);
+            /*title = g_strconcat((c->iconic ? c->icon_title : c->title),
                                 " - ",
                                 (p->iconic ? p->icon_title : p->title),
                                 NULL);
-*/
-
+            */
         icon_popup_show(focus_cycle_popup,
                         (title ? title :
                          (c->iconic ? c->icon_title : c->title)),

1ed2670b891929be289b4099ee752a592e8f123b
Mikael Magnusson <mikachu@comhem.se>
2004-08-22 00:23:44 +0000

    revert patch, doesnt work

--- openbox/focus.c
+++ openbox/focus.c
@@ -320,7 +320,7 @@ ObClient* focus_fallback_target(ObFocusFallbackType type)
 
 void focus_fallback(ObFocusFallbackType type)
 {
-    ObClient *new = focus_fallback_target(type);
+    ObClient *new;
 
     /* unfocus any focused clients.. they can be focused by Pointer events
        and such, and then when I try focus them, I won't get a FocusIn event
@@ -328,7 +328,7 @@ void focus_fallback(ObFocusFallbackType type)
     */
     focus_set_client(NULL);
 
-    if (new)
+    if ((new = focus_fallback_target(type)))
         client_focus(new);
 }
 

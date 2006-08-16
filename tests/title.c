f532c9a1de209e8a1bd4ae2c116286b8216dcf52
Mikael Magnusson <mikachu@comhem.se>
2006-08-16 16:16:21 +0000

    update the title test program to allow combinations of WM_NAME/_NET_WM_NAME and STRING/UTF8_STRING (netwm+!utf8 is invalid, wm+utf8 is not but does not work in openbox currently)

--- tests/title.c
+++ tests/title.c
@@ -27,6 +27,7 @@ int main (int argc, char **argv) {
   int        x=10,y=10,h=100,w=400;
   XSizeHints size;
   XTextProperty name;
+  Atom nameprop,nameenc;
 
   if (argc < 2) return 1;
 
@@ -37,17 +38,28 @@ int main (int argc, char **argv) {
     return 0;
   }
 
+  if (argc > 2)
+    nameprop = XInternAtom(display,argv[2],False);
+  else
+    nameprop = XInternAtom(display,"WM_NAME",False);
+  if (argc > 3)
+    nameenc = XInternAtom(display,argv[3],False);
+  else
+    nameenc = XInternAtom(display,argv[3],False);
+
   win = XCreateWindow(display, RootWindow(display, 0),
 		      x, y, w, h, 10, CopyFromParent, CopyFromParent,
 		      CopyFromParent, 0, NULL);
 
   XSetWindowBackground(display,win,WhitePixel(display,0)); 
 
-  XStringListToTextProperty(&argv[1], 1, &name);
-  XSetWMName(display, win, &name);
-  
-  XMapWindow(display, win);
+//  XStringListToTextProperty(&argv[1], 1, &name);
+//  XSetWMName(display, win, &name);
+  XChangeProperty(display, win, nameprop, nameenc, 8,
+                  PropModeAppend, argv[1], strlen(argv[1]));
+
   XFlush(display);
+  XMapWindow(display, win);
 
   XSelectInput(display, win, ExposureMask | StructureNotifyMask);
 

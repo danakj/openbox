aa852556c0bc12e41a24da01df031d1eb886f9fa
Dana Jansens <danakj@orodu.net>
2007-03-03 21:10:15 +0000

    let the configuration file changes affect windows' decroations and functionality on reconfigure (fix keepBorder bug 2841

--- openbox/openbox.c
+++ openbox/openbox.c
@@ -287,6 +287,9 @@ gint main(gint argc, gchar **argv)
                 /* redecorate all existing windows */
                 for (it = client_list; it; it = g_list_next(it)) {
                     ObClient *c = it->data;
+                    /* the new config can change the window's decorations */
+                    client_setup_decor_and_functions(c);
+                    /* redraw the frames */
                     frame_adjust_area(c->frame, TRUE, TRUE, FALSE);
                 }
             }

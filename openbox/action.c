396ec50063421bd225145298d4be97884a1e47b2
Mikael Magnusson <mikachu@comhem.se>
2007-04-12 21:10:16 +0000

    mysterious typo

--- openbox/action.c
+++ openbox/action.c
@@ -1122,7 +1122,7 @@ void action_execute(union ActionData *data)
                     g_error_free(e);
                     sn_spawn_cancel();
                 }
-                setenv("DESKTOP_STARTUP_ID");
+                unsetenv("DESKTOP_STARTUP_ID");
                 g_free(program);
                 g_strfreev(argv);
             } else {

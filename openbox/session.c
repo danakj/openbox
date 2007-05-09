a97b2f75fb6c0180098b0fef04cbd7ab1aa8ae34
Dana Jansens <danakj@orodu.net>
2007-05-09 21:17:31 +0000

    mem leak

--- openbox/session.c
+++ openbox/session.c
@@ -394,6 +394,7 @@ static void sm_save_yourself(SmcConn conn, SmPointer data, gint save_type,
 
     if (!SmcRequestSaveYourselfPhase2(conn, sm_save_yourself_2, savedata)) {
         ob_debug_type(OB_DEBUG_SM, "Requst for phase 2 failed\n");
+        g_free(savedata);
         SmcSaveYourselfDone(conn, FALSE);
     }
 }

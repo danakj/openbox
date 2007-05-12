fb32040b4ceb558fafd874a1f5ad0127bb97782b
Dana Jansens <danakj@orodu.net>
2007-05-12 22:21:05 +0000

    copy debian from trunk

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(openbox):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="openbox" command="/usr/bin/openbox"
---
+++ debian/openbox-default.ex
@@ -0,0 +1,10 @@
+# Defaults for openbox initscript
+# sourced by /etc/init.d/openbox
+# installed at /etc/default/openbox by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""

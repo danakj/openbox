c1fd5a7ab2495d4508998b936913e15c3f5c79ca
Dana Jansens <danakj@orodu.net>
2007-05-14 02:07:42 +0000

    merge r6520-6522 from trunk

--- openbox/dock.c
+++ openbox/dock.c
@@ -28,7 +28,7 @@
 #include "render/theme.h"
 
 #define DOCK_EVENT_MASK (ButtonPressMask | ButtonReleaseMask | \
-                         EnterWindowMask | LeaveWindowMask | FocusChangeMask)
+                         EnterWindowMask | LeaveWindowMask)
 #define DOCKAPP_EVENT_MASK (StructureNotifyMask)
 
 static ObDock *dock;

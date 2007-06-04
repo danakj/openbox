f1452eef54192893847a57310197b0b35b3bef98
Dana Jansens <danakj@orodu.net>
2007-06-04 19:16:09 +0000

    merge 7342-45 from trunk

--- openbox/place.c
+++ openbox/place.c
@@ -268,7 +268,10 @@ typedef enum
 } ObSmartType;
 
 #define SMART_IGNORE(placer, c) \
-    (placer == c || c->shaded || !client_normal(c) || !c->frame->visible || \
+    (placer == c || c->shaded || !c->frame->visible || \
+     c->type == OB_CLIENT_TYPE_SPLASH || c->type == OB_CLIENT_TYPE_DESKTOP || \
+     ((c->type == OB_CLIENT_TYPE_MENU || c->type == OB_CLIENT_TYPE_TOOLBAR) &&\
+      client_has_parent(c)) || \
      (c->desktop != DESKTOP_ALL && \
       c->desktop != (placer->desktop == DESKTOP_ALL ? \
                      screen_desktop : placer->desktop)))

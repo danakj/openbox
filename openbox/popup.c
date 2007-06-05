7736fcc4b69af5847d711e90d008e05b5c4c4f26
Dana Jansens <danakj@orodu.net>
2007-06-05 16:46:31 +0000

    center text in popups when the popu is sized to the text width

--- openbox/popup.c
+++ openbox/popup.c
@@ -183,8 +183,11 @@ void popup_delay_show(ObPopup *self, gulong usec, gchar *text)
         texth = self->h - emptyy;
     h = texth * self->iconhm + emptyy;
 
-    if (self->textw)
+    if (self->textw) {
+        self->a_text->texture[0].data.text.justify = RR_JUSTIFY_LEFT;
         textw = self->textw;
+    } else
+        self->a_text->texture[0].data.text.justify = RR_JUSTIFY_CENTER;
 
     iconx = textx = l + ob_rr_theme->paddingx;
 

2827b2ce899fb28ab05b369c90126f053d021b6f
Dana Jansens <danakj@orodu.net>
2007-07-11 20:00:55 +0000

    let moveto action resize

--- openbox/actions/moveto.c
+++ openbox/actions/moveto.c
@@ -11,6 +11,8 @@ typedef struct {
     gboolean yopposite;
     gint x;
     gint y;
+    gint w;
+    gint h;
     gint monitor;
 } Options;
 
@@ -35,6 +37,8 @@ static gpointer setup_func(ObParseInst *i, xmlDocPtr doc, xmlNodePtr node)
     o = g_new0(Options, 1);
     o->x = G_MININT;
     o->y = G_MININT;
+    o->w = G_MININT;
+    o->h = G_MININT;
     o->monitor = -1;
 
     if ((n = parse_find_node("x", node))) {
@@ -67,6 +71,11 @@ static gpointer setup_func(ObParseInst *i, xmlDocPtr doc, xmlNodePtr node)
         g_free(s);
     }
 
+    if ((n = parse_find_node("width", node)))
+        o->w = parse_int(doc, n) - 1;
+    if ((n = parse_find_node("height", node)))
+        o->h = parse_int(doc, n) - 1;
+
     if ((n = parse_find_node("monitor", node)))
         o->monitor = parse_int(doc, n) - 1;
 
@@ -98,20 +107,37 @@ static gboolean run_func(ObActionsData *data, gpointer options)
         area = screen_area(c->desktop, mon, NULL);
         carea = screen_area(c->desktop, cmon, NULL);
 
+        w = o->w;
+        if (w == G_MININT) w = c->area.width;
+
+        h = o->h;
+        if (h == G_MININT) h = c->area.height;
+
+        /* it might not be able to resize how they requested, so find out what
+           it will actually be resized to */
+        x = c->area.x;
+        y = c->area.y;
+        client_try_configure(c, &x, &y, &w, &h, &lw, &lh, TRUE);
+
+        /* get the frame's size */
+        w += c->frame->size.left + c->frame->size.right;
+        h += c->frame->size.top + c->frame->size.bottom;
+
         x = o->x;
-        if (o->xcenter) x = (area->width - c->frame->area.width) / 2;
+        if (o->xcenter) x = (area->width - w) / 2;
         else if (x == G_MININT) x = c->frame->area.x - carea->x;
-        else if (o->xopposite) x = area->width - c->frame->area.width;
+        else if (o->xopposite) x = area->width - w;
         x += area->x;
 
         y = o->y;
-        if (o->ycenter) y = (area->height - c->frame->area.height) / 2;
+        if (o->ycenter) y = (area->height - h) / 2;
         else if (y == G_MININT) y = c->frame->area.y - carea->y;
-        else if (o->yopposite) y = area->height - c->frame->area.height;
+        else if (o->yopposite) y = area->height - h;
         y += area->y;
 
-        w = c->area.width;
-        h = c->area.height;
+        /* get the client's size back */
+        w -= c->frame->size.left + c->frame->size.right;
+        h -= c->frame->size.top + c->frame->size.bottom;        
 
         frame_frame_gravity(c->frame, &x, &y); /* get the client coords */
         client_try_configure(c, &x, &y, &w, &h, &lw, &lh, TRUE);

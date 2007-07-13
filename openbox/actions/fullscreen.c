6c538915c86b0330bd78e8d8f3e77d78cf0c5680
Dana Jansens <danakj@orodu.net>
2007-07-13 11:24:38 -0400

    rename fullscreen back to togglefullscreen

--- openbox/actions/fullscreen.c
+++ openbox/actions/fullscreen.c
@@ -1,24 +1,21 @@
 #include "openbox/actions.h"
 #include "openbox/client.h"
 
-static gboolean run_func(ObActionsData *data, gpointer options);
+static gboolean run_func_toggle(ObActionsData *data, gpointer options);
 
 void action_fullscreen_startup()
 {
-    actions_register("Fullscreen",
-                     NULL, NULL,
-                     run_func,
+    actions_register("ToggleFullscreen", NULL, NULL, run_func_toggle,
                      NULL, NULL);
 }
 
 /* Always return FALSE because its not interactive */
-static gboolean run_func(ObActionsData *data, gpointer options)
+static gboolean run_func_toggle(ObActionsData *data, gpointer options)
 {
     if (data->client) {
         actions_client_move(data, TRUE);
         client_fullscreen(data->client, !data->client->fullscreen);
         actions_client_move(data, FALSE);
     }
-
     return FALSE;
 }

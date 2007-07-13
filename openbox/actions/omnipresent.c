07df19094906256f930b614b9d16f9ea0a7211aa
Dana Jansens <danakj@orodu.net>
2007-07-13 11:24:26 -0400

    don't move focus if the window appears or disappears

--- openbox/actions/omnipresent.c
+++ openbox/actions/omnipresent.c
@@ -13,9 +13,12 @@ void action_omnipresent_startup()
 /* Always return FALSE because its not interactive */
 static gboolean run_func_toggle(ObActionsData *data, gpointer options)
 {
-    if (data->client)
+    if (data->client) {
+        actions_client_move(data, TRUE);
         client_set_desktop(data->client,
                            data->client->desktop == DESKTOP_ALL ?
                            screen_desktop : DESKTOP_ALL, FALSE, TRUE);
+        actions_client_move(data, FALSE);
+    }
     return FALSE;
 }

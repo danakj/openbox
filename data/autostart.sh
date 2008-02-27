120bab667f22b211f90ce105eba123a8b5e3f7f1
Dana Jansens <danakj@orodu.net>
2008-02-26 21:17:26 -0500

    add xce-mcs-manager to the autostart.sh - run it if gsd is not found

--- data/autostart.sh
+++ data/autostart.sh
@@ -22,10 +22,13 @@ if which dbus-launch >/dev/null && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
 fi
 
 # Make GTK apps look and behave how they were set up in the gnome config tools
-if which /usr/libexec/gnome-settings-daemon >/dev/null; then
+if test -x /usr/libexec/gnome-settings-daemon >/dev/null; then
   /usr/libexec/gnome-settings-daemon &
 elif which gnome-settings-daemon >/dev/null; then
   gnome-settings-daemon &
+# Make GTK apps look and behave how they were set up in the XFCE config tools
+elif which xfce-mcs-manager >/dev/null; then
+  xfce-mcs-manager n &
 fi
 
 # Preload stuff for KDE apps

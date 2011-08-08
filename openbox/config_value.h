ca408b7bbd8b591cd32a50902d406cebca996f4c
Dana Jansens <danakj@orodu.net>
2011-08-08 18:02:57 -0400

    Remove complicated stuff from config_value.c that we're not using at the moment.

--- openbox/config_value.h
+++ openbox/config_value.h
@@ -24,40 +24,6 @@ struct _GravityCoord;
 struct _ObActionList;
 
 typedef struct _ObConfigValue ObConfigValue;
-typedef struct _ObConfigValueEnum ObConfigValueEnum;
-
-struct _ObConfigValueEnum {
-    const gchar *name;
-    guint value;
-};
-
-typedef enum {
-    OB_CONFIG_VALUE_STRING,
-    OB_CONFIG_VALUE_BOOLEAN,
-    OB_CONFIG_VALUE_ENUM,
-    OB_CONFIG_VALUE_INTEGER,
-    OB_CONFIG_VALUE_FRACTION,
-    OB_CONFIG_VALUE_GRAVITY_COORD,
-    OB_CONFIG_VALUE_STRING_LIST,
-    OB_CONFIG_VALUE_ACTIONLIST,
-    NUM_OB_CONFIG_VALUE_TYPES
-} ObConfigValueDataType;
-
-/*! This holds a pointer to one of the possible types in ObConfigValueType. */
-typedef union _ObConfigValueDataPtr {
-    const gpointer *pointer; /*!< Generic pointer */
-    const gchar **string;
-    gboolean *boolean;
-    guint *integer;
-    guint *enumeration;
-    struct {
-        guint numer;
-        guint denom;
-    } *fraction;
-    struct _GravityCoord *coord;
-    const gchar *const**list;
-    struct _ObActionList **actions;
-} ObConfigValueDataPtr;
 
 void config_value_ref(ObConfigValue *v);
 void config_value_unref(ObConfigValue *v);
@@ -81,20 +47,11 @@ gboolean config_value_is_string(const ObConfigValue *v);
 gboolean config_value_is_string_list(const ObConfigValue *v);
 gboolean config_value_is_action_list(const ObConfigValue *v);
 
-/*! Copies the data inside @v to the destination that the pointer @p is
-  pointing to. */
-void config_value_copy_ptr(ObConfigValue *v,
-                           ObConfigValueDataType type,
-                           ObConfigValueDataPtr p,
-                           const ObConfigValueEnum e[]);
-
 /* These ones are valid on a string value */
 
 const gchar* config_value_string(ObConfigValue *v);
 gboolean config_value_bool(ObConfigValue *v);
 guint config_value_int(ObConfigValue *v);
-/*! returns (guint)-1 if an error */
-guint config_value_enum(ObConfigValue *v, const ObConfigValueEnum e[]);
 void config_value_fraction(ObConfigValue *v, gint *numer, gint *denom);
 void config_value_gravity_coord(ObConfigValue *v, struct _GravityCoord *c);
 

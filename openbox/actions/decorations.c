6ea7ae0ab5f8e5816c173c646c0704998be0d661
Dana Jansens <danakj@orodu.net>
2011-08-08 10:51:45 -0400

    Turn the Foo/ToggleFoo/UnFoo actions into a single Foo action with option "set" which is "on", "off", or "toggle"

--- openbox/actions/decorations.c
+++ openbox/actions/decorations.c
@@ -1,74 +1,68 @@
 #include "openbox/action.h"
 #include "openbox/action_list_run.h"
+#include "openbox/config_value.h"
 #include "openbox/client.h"
 #include "openbox/client_set.h"
 
-static gboolean run_func_on(const ObClientSet *set,
-                            const ObActionListRun *data, gpointer options);
-static gboolean run_func_off(const ObClientSet *set,
-                             const ObActionListRun *data, gpointer options);
-static gboolean run_func_toggle(const ObClientSet *set,
-                                const ObActionListRun *data, gpointer options);
+typedef struct {
+    gboolean toggle;
+    gboolean set;
+} Options;
+
+static gpointer setup_func(GHashTable *config);
+static void free_func(gpointer o);
+static gboolean run_func(const ObClientSet *set,
+                         const ObActionListRun *data, gpointer options);
 
 void action_decorations_startup(void)
 {
     action_register("Decorate", OB_ACTION_DEFAULT_FILTER_SINGLE,
-                    NULL, NULL, run_func_on);
-    action_register("Undecorate", OB_ACTION_DEFAULT_FILTER_SINGLE,
-                    NULL, NULL, run_func_off);
-    action_register("ToggleDecorations", OB_ACTION_DEFAULT_FILTER_SINGLE,
-                    NULL, NULL, run_func_toggle);
+                    setup_func, free_func, run_func);
 }
 
-static gboolean each_on(ObClient *c, const ObActionListRun *data, gpointer o)
+static gpointer setup_func(GHashTable *config)
 {
-    client_set_undecorated(data->target, FALSE);
-    return TRUE;
-}
+    ObConfigValue *v;
+    Options *o;
 
-/* Always return FALSE because its not interactive */
-static gboolean run_func_on(const ObClientSet *set,
-                            const ObActionListRun *data, gpointer options)
-{
-    if (!client_set_is_empty(set)) {
-        action_client_move(data, TRUE);
-        client_set_run(set, data, each_on, options);
-        action_client_move(data, FALSE);
+    o = g_slice_new0(Options);
+    o->toggle = TRUE;
+    o->set = FALSE;
+
+    v = g_hash_table_lookup(config, "set");
+    if (v && config_value_is_string(v)) {
+        const gchar *s = config_value_string(v);
+        if (!g_ascii_strcasecmp(s, "on"))
+            o->toggle = FALSE;
+        else if (!g_ascii_strcasecmp(s, "off")) {
+            o->toggle = FALSE;
+            o->set = TRUE;
+        }
     }
-    return FALSE;
+
+    return o;
 }
 
-static gboolean each_off(ObClient *c, const ObActionListRun *data, gpointer o)
+static void free_func(gpointer o)
 {
-    client_set_undecorated(data->target, TRUE);
+    g_slice_free(Options, o);
+}
+
+static gboolean each_run(ObClient *c, const ObActionListRun *data,
+                         gpointer options)
+{
+    Options *o = options;
+    client_set_undecorated(data->target, o->toggle ? !c->undecorated : o->set);
     return TRUE;
 }
 
 /* Always return FALSE because its not interactive */
-static gboolean run_func_off(const ObClientSet *set,
-                             const ObActionListRun *data, gpointer options)
+static gboolean run_func(const ObClientSet *set,
+                         const ObActionListRun *data, gpointer options)
 {
     if (!client_set_is_empty(set)) {
         action_client_move(data, TRUE);
-        client_set_run(set, data, each_off, options);
-        action_client_move(data, FALSE);
-    }
-    return FALSE;
-}
-
-static gboolean each_flip(ObClient *c, const ObActionListRun *data, gpointer o)
-{
-    client_set_undecorated(data->target, !c->undecorated);
-    return TRUE;
-}
-
-/* Always return FALSE because its not interactive */
-static gboolean run_func_toggle(const ObClientSet *set,
-                                const ObActionListRun *data, gpointer options)
-{
-    if (!client_set_is_empty(set)) {
-        action_client_move(data, TRUE);
-        client_set_run(set, data, each_flip, options);
+        client_set_run(set, data, each_run, options);
         action_client_move(data, FALSE);
     }
     return FALSE;
--- openbox/actions/maximize.c
+++ openbox/actions/maximize.c
@@ -13,25 +13,19 @@ typedef enum {
 
 typedef struct {
     MaxDirection dir;
+    gboolean toggle;
+    gboolean set;
 } Options;
 
 static gpointer setup_func(GHashTable *config);
 static void free_func(gpointer o);
-static gboolean run_func_on(const ObClientSet *set,
-                            const ObActionListRun *data, gpointer options);
-static gboolean run_func_off(const ObClientSet *set,
-                             const ObActionListRun *data, gpointer options);
-static gboolean run_func_toggle(const ObClientSet *set,
-                                const ObActionListRun *data, gpointer options);
+static gboolean run_func(const ObClientSet *set,
+                         const ObActionListRun *data, gpointer options);
 
 void action_maximize_startup(void)
 {
     action_register("Maximize", OB_ACTION_DEFAULT_FILTER_SINGLE,
-                    setup_func, free_func, run_func_on);
-    action_register("Unmaximize", OB_ACTION_DEFAULT_FILTER_SINGLE,
-                    setup_func, free_func, run_func_off);
-    action_register("ToggleMaximize", OB_ACTION_DEFAULT_FILTER_SINGLE,
-                    setup_func, free_func, run_func_toggle);
+                    setup_func, free_func, run_func);
 }
 
 static gpointer setup_func(GHashTable *config)
@@ -41,6 +35,8 @@ static gpointer setup_func(GHashTable *config)
 
     o = g_slice_new0(Options);
     o->dir = BOTH;
+    o->toggle = TRUE;
+    o->set = TRUE;
 
     v = g_hash_table_lookup(config, "dir");
     if (v && config_value_is_string(v)) {
@@ -52,6 +48,16 @@ static gpointer setup_func(GHashTable *config)
                  !g_ascii_strcasecmp(s, "horz"))
             o->dir = HORZ;
     }
+    v = g_hash_table_lookup(config, "set");
+    if (v && config_value_is_string(v)) {
+        const gchar *s = config_value_string(v);
+        if (!g_ascii_strcasecmp(s, "on"))
+            o->toggle = FALSE;
+        else if (!g_ascii_strcasecmp(s, "off")) {
+            o->toggle = FALSE;
+            o->set = FALSE;
+        }
+    }
 
     return o;
 }
@@ -61,74 +67,25 @@ static void free_func(gpointer o)
     g_slice_free(Options, o);
 }
 
-static gboolean each_on(ObClient *c, const ObActionListRun *data,
+static gboolean each_run(ObClient *c, const ObActionListRun *data,
                         gpointer options)
 {
     Options *o = options;
-    if (data->target) {
-        action_client_move(data, TRUE);
-        client_maximize(data->target, TRUE, o->dir);
-        action_client_move(data, FALSE);
-    }
-    return TRUE;
-}
-
-/* Always return FALSE because its not interactive */
-static gboolean run_func_on(const ObClientSet *set,
-                            const ObActionListRun *data, gpointer options)
-{
-    if (!client_set_is_empty(set)) {
-        action_client_move(data, TRUE);
-        client_set_run(set, data, each_on, options);
-        action_client_move(data, FALSE);
-    }
-    return FALSE;
-}
-
-static gboolean each_off(ObClient *c, const ObActionListRun *data,
-                         gpointer options)
-{
-    Options *o = options;
-    if (data->target) {
-        action_client_move(data, TRUE);
-        client_maximize(data->target, FALSE, o->dir);
-        action_client_move(data, FALSE);
-    }
-    return TRUE;
-}
-
-/* Always return FALSE because its not interactive */
-static gboolean run_func_off(const ObClientSet *set,
-                             const ObActionListRun *data, gpointer options)
-{
-    if (!client_set_is_empty(set)) {
-        action_client_move(data, TRUE);
-        client_set_run(set, data, each_off, options);
-        action_client_move(data, FALSE);
-    }
-    return FALSE;
-}
-
-static gboolean each_toggle(ObClient *c, const ObActionListRun *data,
-                            gpointer options)
-{
-    Options *o = options;
     gboolean toggle;
-    toggle = ((o->dir == HORZ && !data->target->max_horz) ||
-              (o->dir == VERT && !data->target->max_vert) ||
-              (o->dir == BOTH &&
-               !(data->target->max_horz && data->target->max_vert)));
-    client_maximize(data->target, toggle, o->dir);
+    toggle = ((o->dir == HORZ && !c->max_horz) ||
+              (o->dir == VERT && !c->max_vert) ||
+              (o->dir == BOTH && !(c->max_horz && c->max_vert)));
+    client_maximize(data->target, (o->toggle ? toggle : o->set), o->dir);
     return TRUE;
 }
 
 /* Always return FALSE because its not interactive */
-static gboolean run_func_toggle(const ObClientSet *set,
-                                const ObActionListRun *data, gpointer options)
+static gboolean run_func(const ObClientSet *set,
+                         const ObActionListRun *data, gpointer options)
 {
     if (!client_set_is_empty(set)) {
         action_client_move(data, TRUE);
-        client_set_run(set, data, each_toggle, options);
+        client_set_run(set, data, each_run, options);
         action_client_move(data, FALSE);
     }
     return FALSE;
